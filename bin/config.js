56eb87dce146c90467736e41c31ddf70f8577fdb
Abiral <abi.sthapit@gmail.com>
2018-01-05 01:22:48 +0545

    added prod and dev config

---
+++ bin/config.js
@@ -0,0 +1,9 @@
+var config = {
+    port_number: 5500,
+    //database
+    db: 'mongodb://abrialstha:test123@localhost:27017/qa_worksheet'
+    // Server_db
+    // db: 'mongodb://root:Kp2322014@127.0.0.1:2727/worksheet?authSource=admin'
+}
+
+module.exports = config;
