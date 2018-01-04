d8cf3f8855ef37adb941294a1c2d5360d36e5590
Abiral <abi.sthapit@gmail.com>
2018-01-05 01:26:44 +0545

    added routes for login and logout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var password = require('../bin/password');
+
+//Controllers
+var auth = require('../controllers/authController');
+
+//Initialize Router
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'QA_WD' });
 });
+/* Login To App */
+router.post('/login', auth.login)
+/* Logout From App */
+router.use('/logout', password)
+router.delete('/logout', auth.logout)
 
 module.exports = router;
