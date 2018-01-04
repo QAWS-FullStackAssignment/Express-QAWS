3aec9deedc078de5cf8175dc4504e1c288c31345
Abiral <abi.sthapit@gmail.com>
2018-01-05 01:26:16 +0545

    basic auth check

---
+++ bin/password.js
@@ -0,0 +1,21 @@
+var Session = require('../models/Session')
+
+var auth = function (req, res, next) {
+    let authtoken = req.headers.authorization;
+    Session.findOne({ "token": authtoken }, (err, session) => {
+        if (err)
+            return res.status(500).json({ status: 'failed', err });
+        else {
+            //console.log(session)
+            if (session)
+                next();
+            else
+                res.status(400).json({
+                    auth: false,
+                    msg: "you are not authorized"
+                });
+        }
+    })
+}
+
+module.exports = auth
