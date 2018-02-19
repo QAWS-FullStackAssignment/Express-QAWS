37306600ca3e8244f1f573493739205785fdeb26
Abiral <abi.sthapit@gmail.com>
2018-02-19 16:04:53 +0545

    remove server config

--- bin/config.js
+++ bin/config.js
@@ -2,8 +2,7 @@ var config = {
     port_number: 5500,
     //database
     db: 'mongodb://abrialstha:test123@localhost:27017/qa_worksheet'
-    // Server_db
-    // db: 'mongodb://root:Kp2322014@127.0.0.1:2727/worksheet?authSource=admin'
+    
 }
 
 module.exports = config;
